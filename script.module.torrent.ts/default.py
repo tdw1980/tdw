1662de7f8f7790ae3ca720021f6576664b4a7a70
tdw1980 <tdw19111980>
2016-03-31 11:51:50 +0300

    310316

--- script.module.torrent.ts/default.py
+++ script.module.torrent.ts/default.py
@@ -1,43 +1,12 @@
-﻿import sys,os,time,threading,random,re
-import xbmc
-import xbmcgui
-import xbmcplugin
-import xbmcvfs
-import socket
+# -*- coding: utf-8 -*-
+
+import sys
+import gc
 import xbmcaddon
-import cookielib
-import urllib2,urllib
-import json,random,httplib
 
-settings = xbmcaddon.Addon(id='script.module.torrent.ts')
-language = settings.getLocalizedString
-version = "0.0.1"
-plugin = "torrent.ts-" + version
+__settings__ = xbmcaddon.Addon(id='script.module.torrent.ts')
+__plugin__ = __settings__.getAddonInfo('name')
+__root__ = __settings__.getAddonInfo('path')
 
-print plugin
-
-Addon = xbmcaddon.Addon(id='script.module.torrent.ts')
-Addon.setSetting('active','0')
-
-pcook= Addon.getSetting('pcook')
-
-if not pcook: 
-    pcook=str(random.randint(0, 0x7fffffff))
-    Addon.setSetting('pcook',pcook)
-    try:
-        cc=httplib.HTTPSConnection('api.parse.com')
-        cc.connect()
-        params={}
-        params['user']=pcook
-        params['action']='install'
-        cc.request('POST', '/1/classes/Installations',
-            json.dumps(params),
-            {
-            "X-Parse-Application-Id":"FSKJs49Ljj9ibnf9Lyq2tFGDdT1Z6lr1wTEiuLPB",
-            "X-Parse-REST-API-Key":"AfsszHeFjXQVD642LAuso0DjDYbCJOyeM6Qj1BiQ",
-            "Content-Type":"application/json"
-            })
-        txt=cc.getresponse().read()
-        cc.close()
-        print txt
-    except: pass
+if __name__ == "__main__":
+    gc.collect()
